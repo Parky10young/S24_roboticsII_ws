c69cae09ea0a4dddedd00a865cac6b978bcb6e98
ZiqiaoFang <brandonfang412@gmail.com>
2024-03-26 17:26:16 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -82,8 +82,8 @@ class TrackingNode(Node):
         
         # PID controller parameters
         self.kp_linear = 1.0  # Proportional gain for linear velocity
-        self.ki_linear = 0.01  # Integral gain for linear velocity
-        self.kd_linear = 0.1  # Derivative gain for linear velocity
+        self.ki_linear = 0.0  # Integral gain for linear velocity
+        self.kd_linear = 0.0  # Derivative gain for linear velocity
         self.kp_angular = 1.0  # Proportional gain for angular velocity
         self.prev_error_linear = 0.0
         self.integral_linear = 0.0
@@ -163,12 +163,16 @@ class TrackingNode(Node):
     def controller(self):
             # Get the current object pose in the robot base_footprint frame
             current_object_pose = self.get_current_object_pose()
+
+            # Set max linear speed and angular speed
+            linear_max = 0.25
+            angular_max = 0.75
             
             # Calculate the x-distance error
             x_distance = current_object_pose[0]
             x_error = self.desired_x_distance - x_distance
             
-            print("distance error",x_error)
+            
 
             # PID controller for linear velocity
             self.integral_linear += x_error
@@ -180,11 +184,18 @@ class TrackingNode(Node):
             y_distance = current_object_pose[1]
             angular_error = math.atan2(y_distance, x_distance)
 
-            print("angular error",angular_error)
+            
             
             # P controller for angular velocity
             angular_output = self.kp_angular * angular_error
+
+            linear_output = np.clip(linear_output,-linear_max,linear_max)
+            angular_output = np.clip(angular_output, -angular_max, angular_max)
             
+            print("distance error",x_error,"distance output",linear_output)
+
+            print("angular error",angular_error,"angular_output",angular_output)
+
             # Update the control velocity command
             cmd_vel = Twist()
             cmd_vel.linear.x = linear_output
